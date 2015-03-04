2d75757cbd9c572dfe16f8edf1e5160ae45ef50c
kazancevsm <sunout1303@yandex.ru>
2015-03-05 00:48:01 +0500

    new file

---
+++ 0.1.3/config.php
@@ -0,0 +1,47 @@
+<?php
+/*
++-------------------------------------------------------------------------------+
+	© r107.pro, 2014. Все права защищены.
+	Сайт: http://r107.pro
+	Почта: support@r107.pro
+	Файл: install.php
+	Версия: 0.1
+	Кодировка: utf8
+	Дата: 04.11.2014 05:05:05
+	Автор: © Казанцев Сергей	[Sunout]
++-------------------------------------------------------------------------------+
+	© r107.pro, 2014. All Rights Reserved.
+	Site: http://r107.pro
+	Email: support@r107.pro
+	File: install.php
+	Version: 0.1
+	Charset: utf8
+	Date: 04.11.2014 05:05:05
+	Author: © Казанцев Сергей	[Sunout]
++-------------------------------------------------------------------------------+
+*/
+
+# Файл конфигурации создан автоматически программой установки системы е107
+
+
+$mySQLserver    = 'localhost';
+$mySQLuser      = 'root';
+$mySQLpassword  = '123';
+$mySQLdefaultdb = 'r107';
+$mySQLprefix    = 'pro_';
+//$mySQLcharset может содержать, только 'utf8' или ''
+$mySQLcharset   = 'utf8';
+
+$ADMIN_DIRECTORY     = 'admin/';
+$CUSTOM_DIRECTORY    = 'custom/';
+$FILES_DIRECTORY     = 'files/';
+$IMAGES_DIRECTORY    = 'images/';
+$THEMES_DIRECTORY    = 'themes/';
+$PLUGINS_DIRECTORY   = 'plugins/';
+$HANDLERS_DIRECTORY  = 'handlers/';
+$LANGUAGES_DIRECTORY = 'languages/';
+$HELP_DIRECTORY      = 'docs/help/';
+$DOWNLOADS_DIRECTORY = 'files/downloads/';
+
+
+?>
