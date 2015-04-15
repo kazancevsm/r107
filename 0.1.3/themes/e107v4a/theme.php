97dd431be524761083e177898aebab3601317411
dm <dm@smt46-lab.Dlink>
2015-04-15 11:01:21 +0500

    тема e107v4: Приведён в порядок код, изменение русскоязычной локализации.

--- 0.1.3/themes/e107v4a/theme.php
+++ 0.1.3/themes/e107v4a/theme.php
@@ -46,67 +46,57 @@ $logo = THEME_ABS."images/bullet3.gif";
 $HEADER =
 "
 <table style='width:100%; background-color:#E4E0E0' cellspacing='3' class='topborder'>
-<tr>
-<td style='text-align:left; vertical-align:bottom'>
-{CUSTOM=clock}
-</td>
-<td style='text-align:right'>
-{CUSTOM=search+".THEME_ABS."images/search.png+18+19}
-</td>
-</tr>
+	<tr>
+		<td style='text-align:left; vertical-align:bottom'>
+			{CUSTOM=clock}
+		</td>
+		<td style='text-align:right'>
+			{CUSTOM=search+".THEME_ABS."images/search.png+18+19}
+		</td>
+	</tr>
 </table>
+
 <table style='width:100%;' cellspacing='3' id='header'>
-<tr>
-<td colspan='2' style='text-align:left; vertical-align: middle;'>
-<img src='".THEME_ABS."images/logo.png' alt='' /> [ {SITENAME} ]
-</td>
-<td style='text-align:right'>
-{BANNER}
-</td>
-</tr>
+	<tr>
+		<td colspan='2' style='text-align:left; vertical-align: middle;'>
+			<img src='".THEME_ABS."images/logo.png' alt='' /> [ {SITENAME} ]
+		</td>
+		<td style='text-align:right'>
+			{BANNER}
+		</td>
+	</tr>
 </table>
+
 <table style='width:100%' cellspacing='3'>
-<tr>
-<td style='width:20%;'></td>
-<td style='width:60%;'><img src='".THEME_ABS."images/blank.gif' width='1' height='1' alt='' /></td>
-<td style='width:20%;'></td>
-</tr>
-<tr>
-<td style='width:20%; vertical-align: top;'>
-{SITELINKS=menu}
-{MENU=1}
-</td><td style='width:60%; vertical-align: top;'>";
+	<tr>
+		<td style='width:20%;'></td>
+		<td style='width:60%;'>
+			<img src='".THEME_ABS."images/blank.gif' width='1' height='1' alt='' />
+		</td>
+		<td style='width:20%;'></td>
+	</tr>
+	<tr>
+		<td style='width:20%; vertical-align: top;'>
+			{SITELINKS=menu}
+			{MENU=1}
+		</td>
+		<td style='width:60%; vertical-align: top;'>";
 
 $FOOTER =
-"</td><td style='width:20%; vertical-align:top'>
-{MENU=2}
-</td></tr>
-<tr>
-<td colspan='3' style='text-align:center' class='smalltext'>
-
-{SITEDISCLAIMER}
-<br />
-{THEME_DISCLAIMER}
-</td>
-</tr>
+"
+		</td>
+		<td style='width:20%; vertical-align:top'>
+			{MENU=2}
+		</td>
+	</tr>
+	<tr>
+		<td colspan='3' style='text-align:center' class='smalltext'>
+			{SITEDISCLAIMER}<br />
+			{THEME_DISCLAIMER}
+		</td>
+	</tr>
 </table>
-<div style='text-align:center'>
-<table style='width:100%'>
-<tr>
-<td style='width:30%; vertical-align:top'>
-&nbsp;
-{MENU=3}
-</td>
-<td style='width:40%; vertical-align:top'>
-{MENU=4}
-</td>
-<td style='width:30%; vertical-align:top'>
-&nbsp;
-{MENU=5}
-</td>
-</tr>
-</table>
-</div>";
+";
 
 
 
@@ -116,57 +106,85 @@ function rand_tag(){
 }
 
 //        [newsstyle]
-
 $NEWSSTYLE = "
 <div class='spacer'>
-<table cellpadding='0' cellspacing='0'>
-<tr>
-<td class='captiontopleft'><img src='".THEME_ABS."images/blank.gif' width='24' height='3' alt='' style='display: block;' /></td>
-<td class='captiontopmiddle'><img src='".THEME_ABS."images/blank.gif' width='1' height='3' alt='' style='display: block;' /></td>
-<td class='captiontopright'><img src='".THEME_ABS."images/blank.gif' width='11' height='3' alt='' style='display: block;' /></td>
-</tr>
-</table>
-<table cellpadding='0' cellspacing='0'>
-<tr>
-<td class='captionleft'><img src='".THEME_ABS."images/blank.gif' width='24' height='18' alt='' style='display: block;' /></td>
-<td class='captionbar' style='white-space:nowrap'>
-{STICKY_ICON}{NEWSTITLE}
-</td>
-<td class='captionend'><img src='".THEME_ABS."images/blank.gif' width='12' height='18' alt='' style='display: block;' /></td>
-<td class='captionmain'><img src='".THEME_ABS."images/blank.gif' width='1' height='18' alt='' style='display: block;' /></td>
-<td class='captionright'><img src='".THEME_ABS."images/blank.gif' width='11' height='18' alt='' style='display: block;' /></td>
-</tr>
-</table>
-<table cellpadding='0' cellspacing='0'>
-<tr>
-<td class='bodyleft'><img src='".THEME_ABS."images/blank.gif' width='3' height='1' alt='' style='display: block;' /></td>
-<td class='bodymain'>
-{NEWSBODY}
-{EXTENDED}
-<div class='alttd' style='text-align:right'>
-".LAN_THEME_4." {NEWSAUTHOR} ".LAN_THEME_5." {NEWSDATE}
- |
-{NEWSCOMMENTS}
- |
-{EMAILICON}
-{PRINTICON}
-{PDFICON}
+	<table cellpadding='0' cellspacing='0'>
+		<tr>
+			<td class='captiontopleft'>
+				<img src='".THEME_ABS."images/blank.gif' width='24' height='3' alt='' style='display: block;' />
+			</td>
+			<td class='captiontopmiddle'>
+				<img src='".THEME_ABS."images/blank.gif' width='1' height='3' alt='' style='display: block;' />
+			</td>
+			<td class='captiontopright'>
+				<img src='".THEME_ABS."images/blank.gif' width='11' height='3' alt='' style='display: block;' />
+			</td>
+		</tr>
+	</table>
+	
+	<table cellpadding='0' cellspacing='0'>
+		<tr>
+			<td class='captionleft'>
+				<img src='".THEME_ABS."images/blank.gif' width='24' height='18' alt='' style='display: block;' />
+			</td>
+			<td class='captionbar' style='white-space:nowrap'>
+				{STICKY_ICON}{NEWSTITLE}
+			</td>
+			<td class='captionend'>
+				<img src='".THEME_ABS."images/blank.gif' width='12' height='18' alt='' style='display: block;' />
+			</td>
+			<td class='captionmain'>
+				<img src='".THEME_ABS."images/blank.gif' width='1' height='18' alt='' style='display: block;' />
+			</td>
+			<td class='captionright'>
+				<img src='".THEME_ABS."images/blank.gif' width='11' height='18' alt='' style='display: block;' />
+			</td>
+		</tr>
+	</table>
+
+	<table cellpadding='0' cellspacing='0'>
+		<tr>
+			<td class='bodyleft'>
+				<img src='".THEME_ABS."images/blank.gif' width='3' height='1' alt='' style='display: block;' />
+			</td>
+			<td class='bodymain'>
+				{NEWSBODY}
+				{EXTENDED}
+				<div class='alttd' style='text-align:right'>
+					".LAN_THEME_4." {NEWSAUTHOR} ".LAN_THEME_5." {NEWSDATE}
+ 						|
+ 					{NEWSCOMMENTS}
+ 						|
+					{EMAILICON}
+					{PRINTICON}
+					{PDFICON}
+				</div>
+			</td>
+			<td class='bodyright'>
+				<img src='".THEME_ABS."images/blank.gif' width='3' height='1' alt='' style='display: block;' />
+			</td>
+		</tr>
+	</table>
+
+	<table cellpadding='0' cellspacing='0'>
+		<tr>
+			<td class='bottomleft'>
+				<img src='".THEME_ABS."images/blank.gif' width='10' height='9' alt='' style='display: block;' />
+			</td>
+			
+			<td class='bottommain'>
+				<img src='".THEME_ABS."images/blank.gif' width='1' height='9' alt='' style='display: block;' />
+			</td>
+			<td class='bottomright'>
+				<img src='".THEME_ABS."images/blank.gif' width='10' height='9' alt='' style='display: block;' />
+			</td>
+		</tr>
+	</table>
 </div>
-</td>
-<td class='bodyright'><img src='".THEME_ABS."images/blank.gif' width='3' height='1' alt='' style='display: block;' /></td>
-</tr>
-</table>
-<table cellpadding='0' cellspacing='0'>
-<tr>
-<td class='bottomleft'><img src='".THEME_ABS."images/blank.gif' width='10' height='9' alt='' style='display: block;' /></td>
-<td class='bottommain'><img src='".THEME_ABS."images/blank.gif' width='1' height='9' alt='' style='display: block;' /></td>
-<td class='bottomright'><img src='".THEME_ABS."images/blank.gif' width='10' height='9' alt='' style='display: block;' /></td>
-</tr>
-</table>
-</div>";
+"; //END
 
 
-
+//OTHER STYLES (COMMENT, ICONSTYLE, POSTLINK)
 define("ICONSTYLE", "float: left; border:0");
 define("COMMENTLINK", LAN_THEME_1);
 define("COMMENTOFFSTRING", LAN_THEME_2);
@@ -195,77 +213,105 @@ function tablestyle($caption, $text){
 
         echo "
 <div class='spacer'>
-<table cellpadding='0' cellspacing='0'>
-<tr>
-<td class='captiontopleft'><img src='".THEME_ABS."images/blank.gif' width='24' height='3' alt='' style='display: block;' /></td>
-<td class='captiontopmiddle'><img src='".THEME_ABS."images/blank.gif' width='1' height='3' alt='' style='display: block;' /></td>
-<td class='captiontopright'><img src='".THEME_ABS."images/blank.gif' width='11' height='3' alt='' style='display: block;' /></td>
-</tr>
-</table>
-<table cellpadding='0' cellspacing='0'>
-<tr>
-<td class='captionleft'><img src='".THEME_ABS."images/blank.gif' width='24' height='18' alt='' style='display: block;' /></td>
-<td class='captionbar' style='white-space:nowrap'>".$caption."</td>
-<td class='captionend'><img src='".THEME_ABS."images/blank.gif' width='12' height='18' alt='' style='display: block;' /></td>
-<td class='captionmain'><img src='".THEME_ABS."images/blank.gif' width='1' height='18' alt='' style='display: block;' /></td>
-<td class='captionright'><img src='".THEME_ABS."images/blank.gif' width='11' height='18' alt='' style='display: block;' /></td>
-</tr>
-</table>
-<table cellpadding='0' cellspacing='0'>
-<tr>
-<td class='bodyleft'><img src='".THEME_ABS."images/blank.gif' width='3' height='1' alt='' style='display: block;' /></td>
-<td class='bodymain'>".$text."</td>
-<td class='bodyright'><img src='".THEME_ABS."images/blank.gif' width='3' height='1' alt='' style='display: block;' /></td>
-</tr>
-</table>
-<table cellpadding='0' cellspacing='0'>
-<tr>
-<td class='bottomleft'><img src='".THEME_ABS."images/blank.gif' width='10' height='9' alt='' style='display: block;' /></td>
-<td class='bottommain'><img src='".THEME_ABS."images/blank.gif' width='1' height='9' alt='' style='display: block;' /></td>
-<td class='bottomright'><img src='".THEME_ABS."images/blank.gif' width='10' height='9' alt='' style='display: block;' /></td>
-</tr>
-</table>
+	<table cellpadding='0' cellspacing='0'>
+		<tr>
+			<td class='captiontopleft'>
+				<img src='".THEME_ABS."images/blank.gif' width='24' height='3' alt='' style='display: block;' />
+			</td>
+			<td class='captiontopmiddle'>
+				<img src='".THEME_ABS."images/blank.gif' width='1' height='3' alt='' style='display: block;' />
+			</td>
+			<td class='captiontopright'>
+				<img src='".THEME_ABS."images/blank.gif' width='11' height='3' alt='' style='display: block;' />
+			</td>
+		</tr>
+	</table>
+	
+	<table cellpadding='0' cellspacing='0'>
+		<tr>
+			<td class='captionleft'>
+				<img src='".THEME_ABS."images/blank.gif' width='24' height='18' alt='' style='display: block;' />
+			</td>
+			<td class='captionbar' style='white-space:nowrap'>
+				".$caption."
+			</td>
+			<td class='captionend'>
+				<img src='".THEME_ABS."images/blank.gif' width='12' height='18' alt='' style='display: block;' />
+			</td>
+			<td class='captionmain'>
+				<img src='".THEME_ABS."images/blank.gif' width='1' height='18' alt='' style='display: block;' />
+			</td>
+			<td class='captionright'>
+				<img src='".THEME_ABS."images/blank.gif' width='11' height='18' alt='' style='display: block;' />
+			</td>
+		</tr>
+	</table>
+	
+	<table cellpadding='0' cellspacing='0'>
+		<tr>
+			<td class='bodyleft'>
+				<img src='".THEME_ABS."images/blank.gif' width='3' height='1' alt='' style='display: block;' />
+			</td>
+			<td class='bodymain'>
+				".$text."
+			</td>
+			<td class='bodyright'>
+				<img src='".THEME_ABS."images/blank.gif' width='3' height='1' alt='' style='display: block;' />
+			</td>
+		</tr>
+	</table>
+	
+	<table cellpadding='0' cellspacing='0'>
+		<tr>
+			<td class='bottomleft'>
+				<img src='".THEME_ABS."images/blank.gif' width='10' height='9' alt='' style='display: block;' />
+			</td>
+			<td class='bottommain'>
+				<img src='".THEME_ABS."images/blank.gif' width='1' height='9' alt='' style='display: block;' />
+			</td>
+			<td class='bottomright'>
+				<img src='".THEME_ABS."images/blank.gif' width='10' height='9' alt='' style='display: block;' />
+			</td>
+		</tr>
+	</table>
 </div>
-";
+"; //END
 
 }
 
 
 $COMMENTSTYLE = "
 <div style='text-align:center'>
-<table style='width:100%'>
-<tr>
-<td colspan='2' class='alttd'>
-{SUBJECT}&nbsp;
-<b>
-{USERNAME}
-</b>
- |
- {TIMEDATE}
-</td>
-</tr>
-<tr>
-<td style='width:30%; vertical-align:top'>
-<div class='spacer'>
-{AVATAR}
-</div>
-<span class='smalltext'>
-{LEVEL}
-{COMMENTS}
-<br />
-{JOINED}
-<br />
-{REPLY}
-</span>
-</td>
-<td style='width:70%; vertical-align:top'>
-{COMMENT} {COMMENTEDIT}
-</td>
-</tr>
-</table>
-</div>
-<br />";
-
-
-
+	<table style='width:100%'>
+		<tr>
+			<td colspan='2' class='alttd'>
+				{SUBJECT}&nbsp;
+				<b>
+				{USERNAME}
+				</b>
+				 |
+ 				{TIMEDATE}
+			</td>
+		</tr>
+		<tr>
+			<td style='width:30%; vertical-align:top'>
+				<div class='spacer'>
+					{AVATAR}
+				</div>
+				<span class='smalltext'>
+					{LEVEL}
+					{COMMENTS}
+					<br />
+					{JOINED}
+					<br />
+					{REPLY}
+				</span>
+			</td>
+			<td style='width:70%; vertical-align:top'>
+				{COMMENT} {COMMENTEDIT}
+			</td>
+		</tr>
+	</table>
+</div><br />
+"; //END
 ?>
