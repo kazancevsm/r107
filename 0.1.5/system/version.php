cefeacd0b6326c555d9c7a519074701a5b9b2468
kazancesm <sunout1303@yandex.ru>
2018-07-03 18:38:49 +0500

    new ver 0.1.5

--- 0.1.5/system/version.php
+++ 0.1.5/system/version.php
@@ -2,6 +2,6 @@
 # Version file
 
 if (!defined('e107_INIT')) { exit; }
-$e107info['e107_version'] = "0.1.4 Standart Assembly <br />The author of this Assembly: <a href='http://webmaster.slog.su'>Open Source Group</a>";
+$e107info['e107_version'] = "0.1.5 Standart Assembly <br />The author of this Assembly: <a href='http://webmaster.slog.su'>Open Source Group</a>";
 
 ?>
